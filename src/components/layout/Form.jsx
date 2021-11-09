9113b78db293443759b03dd0a3fb0c0a82bf9317
jefff89 <jeff.abbaszade@gmail.com>
2021-11-09 08:08:36 -0800

    comments are deleted

--- src/components/layout/Form.jsx
+++ src/components/layout/Form.jsx
@@ -6,57 +6,8 @@ import FavContext from "../store/FavContext";
 import { useHistory } from "react-router";
 import Input from "../UI/Input";
 
-// const validReducer = (state, action) => {
-// if (action.type === "NAME") {
-//   return { nameValue: action.val, isValid: action.val.trim().length > 0 };
-// }
-//   return { nameValue: "", isValid: false };
-//   if (action.type === "WRITER") {
-//     return { writerValue: action.val, isValid: action.val.trim().length > 0 };
-//   }
-//   if (action.type === "IMG") {
-//     return { imgValue: action.val, isValid: action.val.trim().length > 0 };
-//   }
-//   if (action.type === "Summary") {
-//     return { summaryValue: action.val, isValid: action.val.trim().length > 0 };
-//   }
-//   return {
-//     nameValue: "",
-//     writerValue: "",
-//     imgValue: "",
-//     summaryValue: "",
-//     isValid: false,
-//   };
-// };
-
 const Form = () => {
-  // const [stateValid, dispatchValidState] = useReducer(validReducer, {
-  //   isValid: null,
-
-  //   nameValue: "",
-  //   writerValue: "",
-  //   imgValue: "",
-  //   summaryValue: "",
-  // });
-  // const onNameChangeHandler = (event) => {
-  //   dispatchValidState({ type: "NAME", val: event.target.value });
-  // };
-  // const onWriterChangeHandler = (event) => {
-  //   dispatchValidState({ type: "WRITER", val: event.target.value });
-  // };
-  // const onImgChangeHandler = (event) => {
-  //   dispatchValidState({ type: "IMG", val: event.target.value });
-  // };
-  // const onSummaryChangeHandler = (event) => {
-  //   dispatchValidState({ type: "SUMMARY", val: event.target.value });
-  // };
-
   const [formIsValid, setFormIsValid] = useState(true);
-
-  // const [nameIsValid, setNameIsValid] = useState(true);
-  // const [writerIsValid, setWriterIsValid] = useState(true);
-  // const [imgIsValid, setImgIsValid] = useState(true);
-  // const [summaryIsValid, setSummaryIsValid] = useState(true);
 
   const favCtx = useContext(FavContext);
   const history = useHistory();
@@ -69,16 +20,8 @@ const Form = () => {
   const ChangeHandler = (event) => {
     if (event.target.value.trim() === 0) {
       setFormIsValid(false);
-      // setNameIsValid(false);
-      // setWriterIsValid(false);
-      // setImgIsValid(false);
-      // setSummaryIsValid(false);
     } else {
       setFormIsValid(true);
-      // setNameIsValid(true);
-      // setWriterIsValid(true);
-      // setImgIsValid(true);
-      // setSummaryIsValid(true);
     }
   };
 
@@ -91,48 +34,35 @@ const Form = () => {
       summary: enteredSummary.current.value,
     };
 
-    // console.log(stateValid);
-    // if (!stateValid.isValid) {
-    //   return;
-    // }
-
     if (values.name.trim().length === 0) {
-      // setNameIsValid(false);
       setFormIsValid(false);
       enteredName.current.focus();
       return;
     } else {
-      // setNameIsValid(true);
       setFormIsValid(true);
     }
 
     if (values.writer.trim().length === 0) {
-      // setWriterIsValid(false);
       setFormIsValid(false);
       enteredWriter.current.focus();
       return;
     } else {
-      // setWriterIsValid(true);
       setFormIsValid(true);
     }
 
     if (values.img.trim().length === 0) {
-      // setImgIsValid(false);
       setFormIsValid(false);
       enteredImg.current.focus();
       return;
     } else {
-      // setImgIsValid(true);
       setFormIsValid(true);
     }
 
     if (values.summary.trim().length === 0) {
-      // setSummaryIsValid(false);
       setFormIsValid(false);
       enteredSummary.current.focus();
       return;
     } else {
-      // setSummaryIsValid(true);
       setFormIsValid(true);
     }
 
@@ -170,19 +100,7 @@ const Form = () => {
             ref: enteredName,
           }}
         />
-        {/* <div
-          className={`${classes["form-control"]} ${
-            !nameIsValid && classes.invalid
-          }`}
-        >
-          <label htmlFor="name">Book Name</label>
-          <input
-            type="text"
-            id="name"
-            ref={enteredName}
-            onChange={ChangeHandler}
-          />
-        </div> */}
+
         <Input
           label="Writer"
           inputs={{
@@ -203,20 +121,13 @@ const Form = () => {
           }}
         />
 
-        <div
-          className={classes["form-control"]}
-          // className={`${classes["form-control"]} ${
-          //   !summaryIsValid && classes.invalid
-          // }`}
-        >
+        <div className={classes["form-control"]}>
           <label htmlFor="summary">Add Summary</label>
           <textarea
             id="summary"
             rows="7"
             ref={enteredSummary}
             onChange={ChangeHandler}
-            // value={stateValid.summaryValue}
-            // onChange={onSummaryChangeHandler}
           ></textarea>
         </div>
         <Button type="submit">Add Summary</Button>
